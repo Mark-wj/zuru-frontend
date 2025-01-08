25b8cf210c47efd148fc83fae2df070e8959bebc
Mark Njenga <marknjenga25@gmail.com>
2025-01-08 22:49:53 +0300

    Create components, pages and installed dependencies

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom/cjs/react-router-dom.min'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="app">
+      
+    </div>
+  )
+}
+
+export default App
